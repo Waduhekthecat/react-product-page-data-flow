672b466d5e78959069dc245472cb4a5a2775d140
Joshua Darby <josh@Joshuas-MacBook-Pro.local>
2026-04-13 19:47:10 -0500

    Build init react product page prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useMemo, useState } from "react";
+import "./App.css";
+
+const product = {
+  name:"",
+  tagline:"",
+  price: 0,
+  variants: ["","",""],
+  highlights: ["","",""],
+  description: "",
+  details: ["","",""]
+}
+
 function App() {
   return (
     <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Hello World</h1>
+      <h1>Product Page</h1>
     </main>
   )
 }
